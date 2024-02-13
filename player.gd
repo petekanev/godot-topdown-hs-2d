c98101602f1a759036ee7b1cf5e8c95ab960e126
Peter Kanev <pete.kanev@gmail.com>
2024-02-13 13:39:22 +0100

    feat: implement player movement and animation

--- player.gd
+++ player.gd
@@ -2,15 +2,43 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+const MAP_BOUNDS = Vector2(1920*2, 1080*2)
 
+var walking_left: bool = false
+var walking_up: bool = false
 
 func process_directional_input():
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = input_direction * SPEED 
+	velocity = input_direction * SPEED
+
+func animate_movement():
+	var animation = "walking"
+	if velocity.length() != 0:
+		walking_left = velocity.x < 0
+		walking_up = velocity.y < 0
+		var walking_down = velocity.y > 0
+		
+		$AnimatedSprite2D.flip_h = walking_left
+
+		if walking_up:
+			animation = "walking_up"
+		elif walking_down:
+			animation = "walking_down"
+			
+		$AnimatedSprite2D.animation = animation
+	else:
+		$AnimatedSprite2D.animation = "idle"
+
+func _ready():
+	$AnimatedSprite2D.animation = "idle"
+	$AnimatedSprite2D.play()
 
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# move
 	process_directional_input()
-
 	move_and_slide()
+	# clamp position to be within world bounds
+	position = position.clamp(Vector2.ZERO, MAP_BOUNDS)
+	
+	# animate
+	animate_movement()
