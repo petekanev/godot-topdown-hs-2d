c71fcf84caf8546dc74c2143f899c33639938886
Peter Kanev <pete.kanev@gmail.com>
2024-02-14 13:46:36 +0100

    fix: enemy movement
    fix: tileset collission

--- enemy.gd
+++ enemy.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 var walking_left: bool = false
+var _next_velocity := Vector2.ZERO
 
 func set_target_position(target: Vector2):
 	$NavigationAgent2D.target_position = target
@@ -23,21 +24,27 @@ func animate_movement():
 func _ready():
 	$AnimatedSprite2D.animation = "idle"
 	$AnimatedSprite2D.play()
+	
+	nav_agent.connect("velocity_computed", Callable(self, "move"))
 
 func _physics_process(delta):
-	nav_agent.target_position = player.position
-
-	var next_direction = position.direction_to(nav_agent.get_next_path_position())
-
-	velocity = next_direction * SPEED
-	nav_agent.set_velocity_forced(velocity)
-
-	#emit_signal("path_changed", [])
-	#prints('pos:', position, 'target pos:', nav_agent.target_position, 'next:', next_direction, 'velocity:', velocity)
-	#prints(nav_agent.get_final_position(), nav_agent.is_navigation_finished(), nav_agent.is_target_reachable())
-
-	move_and_slide()
+	# navigation is never finished - always follow the player
+	#if nav_agent.is_navigation_finished():
+		#return
 	
+	nav_agent.target_position = player.position
+	var next_target_position = nav_agent.get_next_path_position()
+
+	var next_direction = position.direction_to(next_target_position)
+
+	_next_velocity = next_direction * SPEED
+	nav_agent.velocity = _next_velocity
+
 	# animate
 	animate_movement()
+
+func move(computed_velocity: Vector2):
+	velocity = computed_velocity
+	
+	move_and_slide()
 
